4ed5c59620eee32fcbabbcc7bfde0bdbe4855172
Tom Shanley <tom_shanley@msn.com>
2021-04-07 09:56:52 -0700

    Fix consolidated projects so file versions increase on build and isntaller will work

---
+++ CCL/CommonClassesLib5/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CommonClassesLib5")]
+[assembly: AssemblyDescription("Some little helper classes useful with Winforms binding and settings serialization")]
+[assembly: AssemblyCompany("West Leitrim Software")]
+[assembly: AssemblyProduct("CommonClassesLib")]
+[assembly: AssemblyCopyright("Copyright © 2005, 2021")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
